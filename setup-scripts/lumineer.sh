d4d0a99f9ad61cedd065fe99bfe9a0492c6b3cca
Praveen Jindal <praveenjindal62@gmail.com>
2024-11-21 13:00:35 +0000

    updated

---
+++ setup-scripts/lumineer.sh
@@ -0,0 +1,11 @@
+az artifacts universal download \
+    --organization "https://dev.azure.com/slb-swt/" \
+    --feed lightops \
+    --name lumineer-linux-amd64 \
+    --version "*" \
+    --path .
+chmod +x lumineer-linux-amd64
+mkdir -p ~/.local/bin/
+mv lumineer-linux-amd64 ~/.local/bin/lumineer
+lumineer config bash --alias lm > ./lumineer-bash
+sudo mv ./lumineer-bash /etc/bash_completion.d/lumineer
